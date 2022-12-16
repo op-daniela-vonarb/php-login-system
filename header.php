0a17891e1defc007394c3a3f722631751f1b0fad
Daniea von Arb <daniela.vonarb@openpolytechnic.ac.nz>
2022-12-16 13:36:49 +1300

    create header and footer files

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<header>
+    <nav>
+        <div>
+            <h3>Daniela von Arb</h3>
+            <ul class = "menu-main">
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Member+</a></li>
+            </ul>
+        </div>
+        <ul class="menu-member">
+            <li><a href="#" class="header-login-a">LOGOUT</a></li>
+            <li><a href="#">SIGN UP</a></li>
+            <li><a href="#" class="header-login-a">LOGIN</a></li>
+        </ul>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,35 +1,8 @@
 <?php
 
+    include_once 'header.php';
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<header>
-    <nav>
-        <div>
-            <h3>Daniela von Arb</h3>
-            <ul class = "menu-main">
-                <li><a href="index.php">HOME</a></li>
-                <li><a href="#">Products</a></li>
-                <li><a href="#">Member+</a></li>
-            </ul>
-        </div>
-        <ul class="menu-member">
-            <li><a href="#" class="header-login-a">LOGOUT</a></li>
-            <li><a href="#">SIGN UP</a></li>
-            <li><a href="#" class="header-login-a">LOGIN</a></li>
-        </ul>
-    </nav>
-</header>
  
     <section class="index-login">
         <div class="wrapper">
@@ -57,5 +30,11 @@
             </div>
         </div>
     </section>
-</body>
-</html>
+
+<?php
+
+    include_once 'footer.php';
+
+?>
+
+    
